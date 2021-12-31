74a9b44887047c74705027e60322b0388821a297
oslab-csie-ncku <f74081828@gs.ncku.edu.tw>
2021-12-31 15:13:58 +0800

    change_state

--- appbackup.py
+++ appbackup.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 
 machine = TocMachine(
-    states=["user", "state1", "state2", "state3", "state4", "state5", "state6", "state7", "state8", "state9", "state10", "state11", "state12", "state13", "state14", "state15", "state16"],
+    states=["user", "state1", "state2"],
     transitions=[
         {
             "trigger": "advance",
@@ -28,95 +28,11 @@ machine = TocMachine(
             "dest": "state2",
             "conditions": "is_going_to_state2",
         },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state3",
-            "conditions": "is_going_to_state3",
-        },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state4",
-            "conditions": "is_going_to_state4",
-        },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state5",
-            "conditions": "is_going_to_state5",
-        },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state6",
-            "conditions": "is_going_to_state6",
-        },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state7",
-            "conditions": "is_going_to_state7",
-        },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state8",
-            "conditions": "is_going_to_state8",
-        },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state9",
-            "conditions": "is_going_to_state9",
-        },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state10",
-            "conditions": "is_going_to_state10",
-        },
-	{
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state11",
-            "conditions": "is_going_to_state11",
-        },
-	    {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state12",
-            "conditions": "is_going_to_state12",
-        },
-	    {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state13",
-            "conditions": "is_going_to_state13",
-        },
-	    {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state14",
-            "conditions": "is_going_to_state14",
-        },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state15",
-            "conditions": "is_going_to_state15",
-        },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state16",
-            "conditions": "is_going_to_state16",
-        },
-        {"trigger": "go_back", "source": ["state1", "state2", "state3", "state4", "state5", "state6", "state7", "state8", "state9", "state10", "state11", "state12", "state13", "state14", "state15", "state16"], "dest": "user"},
+        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
     ],
     initial="user",
     auto_transitions=False,
-    show_conditions=True, 
+    show_conditions=True,
 )
 
 app = Flask(__name__, static_url_path="")
@@ -188,7 +104,7 @@ def webhook_handler():
         print(f"REQUEST BODY: \n{body}")
         response = machine.advance(event)
         if response == False:
-            send_text_message(event.reply_token, "請確認輸入")
+            send_text_message(event.reply_token, "Not Entering any State")
 
     return "OK"
 
--- fsm.py
+++ fsm.py
@@ -1,7 +1,7 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
-
+from utils import send_text_message, send_image_url
+import random
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
@@ -9,17 +9,70 @@ class TocMachine(GraphMachine):
 
     def is_going_to_state1(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        return text.lower() == "hotlist"
 
     def is_going_to_state2(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        return text.lower() == "recommendation"
+	
+    def is_going_to_state3(self, event):
+        text = event.message.text
+        return text.lower() == "introduction"
+   
+    def is_going_to_state4(self, event):
+        text = event.message.text
+        return text.lower() == "hot 1"
+
+    def is_going_to_state5(self, event):
+        text = event.message.text
+        return text.lower() == "hot 2"
+
+    def is_going_to_state6(self, event):
+        text = event.message.text
+        return text.lower() == "hot 3"
+
+    def is_going_to_state7(self, event):
+        text = event.message.text
+        return text.lower() == "hot 4"
+
+    def is_going_to_state8(self, event):
+        text = event.message.text
+        return text.lower() == "hot 5"
+
+    def is_going_to_state9(self, event):
+        text = event.message.text
+        return text.lower() == "hot 6"
+
+    def is_going_to_state10(self, event):
+        text = event.message.text
+        return text.lower() == "hot 7"
+
+    def is_going_to_state11(self, event):
+        text = event.message.text
+        return text.lower() == "hot 8"
+
+    def is_going_to_state12(self, event):
+        text = event.message.text
+        return text.lower() == "hot 9"
+
+    def is_going_to_state13(self, event):
+        text = event.message.text
+        return text.lower() == "hot 10"
+    def is_going_to_state14(self, event):
+        text = event.message.text
+        return text.lower() == "help"
+    def is_going_to_state15(self, event):
+        text = event.message.text
+        return text.lower() == "fsm"
+    def is_going_to_state16(self, event):
+        text = event.message.text
+        return text.lower() == "add"
 
     def on_enter_state1(self, event):
         print("I'm entering state1")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
+        send_text_message(reply_token, "https://www.billboard.com/charts/hot-100")
         self.go_back()
 
     def on_exit_state1(self):
@@ -27,10 +80,150 @@ class TocMachine(GraphMachine):
 
     def on_enter_state2(self, event):
         print("I'm entering state2")
-
+        music = ['https://www.youtube.com/watch?v=_NGQfFCFUn4', 'https://www.youtube.com/watch?v=GJ63esXbY-M&list=RDGJ63esXbY-M&start_radio=1', 'https://www.youtube.com/watch?v=hxQLe6nlUcA', 'https://www.youtube.com/watch?v=MPbUaIZAaeA', 'https://www.youtube.com/watch?v=pE49WK-oNjU', 'https://www.youtube.com/watch?v=L3_4OXQVNgc']
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
+        send_text_message(reply_token, music[random.randint(0,5)])
         self.go_back()
 
     def on_exit_state2(self):
         print("Leaving state2")
+
+    def on_enter_state3(self, event):
+        print("I'm entering state3")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "本州排行第一的是Taylor Swifty在2020年12月11日發行的全新單曲 willow"+"\n"+"這支單曲收錄於同日發行的最新專輯《evermore》中。《evermore》作為約半年前發行的《folklore》之姊妹專輯但帶有較強烈的秋冬氣息。而willow 與專輯中的其他首歌都是以indie混一些民謠鄉村的風格呈現，這首歌的名字來自抒情詩「生活是一棵柳樹，它隨風而轉」，歌詞更是「Life was willow when they bent right to your wind」呼應，描述了一個女孩在預期之外愛上了一個男孩，即使兩人有許多的差異，卻相互磨合互補，女孩也願意為了愛情奉獻自己與對方相伴")
+        self.go_back()
+
+    def on_exit_state3(self):
+        print("Leaving state3")
+
+    
+    def on_enter_state4(self, event):
+        print("I'm entering state4")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=RsEZmictANA")
+        self.go_back()
+
+    def on_exit_state4(self):
+        print("Leaving state4")
+
+    def on_enter_state5(self, event):
+        print("I'm entering state5")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=aAkMkVFwAoo")
+        self.go_back()
+
+    def on_exit_state5(self):
+        print("Leaving state5")
+
+    def on_enter_state6(self, event):
+        print("I'm entering state5")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=1qYz7rfgLWE")
+        self.go_back()
+
+    def on_exit_state6(self):
+        print("Leaving state6")
+
+    def on_enter_state7(self, event):
+        print("I'm entering state7")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=Z0ajuTaHBtM")
+        self.go_back()
+
+    def on_exit_state7(self):
+        print("Leaving state7")
+
+    def on_enter_state8(self, event):
+        print("I'm entering state8")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=GrAchTdepsU")
+        self.go_back()
+
+    def on_exit_state8(self):
+        print("Leaving state8")
+
+    def on_enter_state9(self, event):
+        print("I'm entering state9")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=DtVxFi9C0RA")
+        self.go_back()
+
+    def on_exit_state9(self):
+        print("Leaving state9")
+
+    def on_enter_state10(self, event):
+        print("I'm entering state10")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=AN_R4pR1hck")
+        self.go_back()
+
+    def on_exit_state10(self):
+        print("Leaving state10")
+
+    def on_enter_state11(self, event):
+        print("I'm entering state11")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=tcYodQoapMg")
+        self.go_back()
+
+    def on_exit_state11(self):
+        print("Leaving state11")
+
+    def on_enter_state12(self, event):
+        print("I'm entering state12")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=gdZLi9oWNZg")
+        self.go_back()
+
+    def on_exit_state12(self):
+        print("Leaving state12")
+
+    def on_enter_state13(self, event):
+        print("I'm entering state13")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "https://www.youtube.com/watch?v=N8NcQzMQN_U")
+        self.go_back()
+
+    def on_exit_state13(self):
+        print("Leaving state13")
+
+    def on_enter_state14(self, event):
+        print("I'm entering state14")
+
+        reply_token = event.reply_token
+        self.go_back()
+
+    def on_exit_state14(self):
+        print("Leaving state14")
+
+    def on_enter_state15(self, event):
+        print("I'm entering state15")
+
+        reply_token = event.reply_token
+        send_image_url(reply_token, 'https://img.onl/yPBTGw')
+        self.go_back()
+
+    def on_exit_state15(self):
+        print("Leaving state15")
+
+    def on_enter_state16(self, event):
+        print("I'm entering state16")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "new state")
+        self.go_back()
+
+    def on_exit_state16(self):
+        print("Leaving state16")
