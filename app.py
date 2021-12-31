577219b972637c2ce0c6f823c12a2e75c40d7c5d
oslab-csie-ncku <f74081828@gs.ncku.edu.tw>
2022-01-01 02:40:29 +0800

    fsm-4

--- app.py
+++ app.py
@@ -207,4 +207,5 @@ def show_fsm():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8000)
+    machine.get_graph().draw("fsm.png", prog="dot", format="png")
     app.run(host="0.0.0.0", port=port, debug=True)
